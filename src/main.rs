06d8cc3e4326731c2ab855c1f8c420af7c7d7f5e
Matt Klass <matt.klass@telosalliance.com>
2024-07-16 14:06:19 +0000

    Run through every object in the game data, and try to parse them both ways. We must fix any issues

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,44 @@
-use std::str::FromStr;
+use std::{fs, io::Read, str::FromStr};
 
+use anyhow::Result;
 use object::{BlocksWalkingData, ClothingData, ClothingType, ColorData, ContainSizeData, DoublePair, InvisHoldingData, MapChanceData, NumSlotsData, NumUsesData, Object, PermanentData, PersonData, SoundData, SoundsData, SpriteData};
 
 mod object;
 
-// The goal is to use Serde to parse something like this:
-/*
-id=8675
-Skull Hat
-containable=1
-containSize=2.000000,vertSlotRot=-0.500000
-permanent=0,minPickupAge=3
-noFlip=0
-sideAccess=0
-heldInHand=0
-blocksWalking=0,leftBlockingRadius=0,rightBlockingRadius=0,drawBehindPlayer=0
-mapChance=0.000000#biomes_0
-heatValue=0
-rValue=0.150000
-person=0,noSpawn=0
-male=0
-deathMarker=0
-homeMarker=0
-floor=0
-floorHugging=0
-foodValue=0
-speedMult=1.000000
-heldOffset=23.000000,29.000000
-clothing=h
-clothingOffset=3.000000,69.999996
-deadlyDistance=0
-useDistance=1
-sounds=153:0.225000,286:0.101010,-1:0.0,-1:0.0
-creationSoundInitialOnly=0
-creationSoundForce=0
-numSlots=0#timeStretch=1.000000
-slotSize=1.000000
-slotsLocked=0
-numSprites=1
-spriteID=101377
-pos=-1.000000,-29.000000
-rot=-0.005000
-hFlip=0
-color=1.000000,1.000000,1.000000
-ageRange=-1.000000,-1.000000
-parent=-1
-invisHolding=0,invisWorn=0,behindSlots=0
-invisCont=0
-headIndex=-1
-bodyIndex=-1
-backFootIndex=-1
-frontFootIndex=-1
-numUses=1,1.000000
-useVanishIndex=-1
-useAppearIndex=-1
-pixHeight=4
-*/
-// Into a struct that allows all of this as optional paramters.
-// We will have to decide what things are required. For our purposes, perhaps just an id and a second line with the name
-// So our parsing will have to make a special case for the second line, where it will just put that into a variable named "name", like how the other lines have variable names like "id" and "clothing"
+fn main() -> Result<()> {
+    // Read each object txt file in the provided directory, and attempt to parse it.
+    // Do if == on the original file data and the FromStr->ToString chain output.
+    // Log errors, but keep going.
+    let object_directory = "../../OneLifeData7/objects/";
+    let object_dir_contents = fs::read_dir(object_directory)?;
+    for entry in object_dir_contents {
+        if let Ok(entry) = entry {
+            // Check if the entry is a file and matches the pattern
+            if let Ok(metadata) = entry.metadata() {
+                if metadata.is_file() {
+                    let file_name = entry.file_name();
+                    let file_name = file_name.to_string_lossy();
 
-fn main() {
+                    if let Some(captures) = regex::Regex::new(r"^(\d+)\.txt$").unwrap().captures(&file_name) {
+                        println!("Found matching file: {}", &file_name);
+
+                        // Read the file into a string
+                        let mut file = fs::File::open(entry.path()).unwrap();
+                        let mut contents = String::new();
+                        file.read_to_string(&mut contents).unwrap();
+
+                        let object = Object::from_str(&contents)?;
+                        let recreated_string = object.to_string();
+                        // assert_eq!(contents, recreated_string);
+                    }
+                }
+            }
+        }
+    }
+    Ok(())
+}
+
+fn original_test() {
     let object_data_str = r#"id=8675
 Skull Hat
 containable=1
@@ -195,10 +173,6 @@ pixHeight=4"#;
         pixHeight: Some(4),
     };
 
-    // println!("\n{object_data_str}\n\n{}\n", object_data_struct.to_string());
-
     assert_eq!(object_data_struct.to_string(), object_data_str);
-
-    println!("from_str struct:\n{:#?}", Object::from_str(object_data_str));
 
 }
