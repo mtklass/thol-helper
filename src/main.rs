f6f7a88c711a15478daae9a192af419a0e50c615
Matt Klass <matt.klass@telosalliance.com>
2024-07-23 17:55:31 +0000

    Fill in README, add check in program for output filename against if wiki-output option was selected

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod object;
 
+use std::io::Read;
+use std::io::Write;
 use std::ops::Div;
 use std::ops::Mul;
 use std::fs::{self, File};
@@ -13,19 +15,28 @@ use glob::glob;
 use object::{ClothingType, Object};
 use serde_json::Value;
 
+const DEFAULT_OUTOUT_FILENAME: &str = "output.json";
+
 #[derive(Parser, Default)]
 #[command(
     author,
     about,
     about = r#"Filter twotech's object data for objects that interest you."#
 )]
+
 pub struct Args {
     // #[arg(short = 'd', long, default_value = "../../TwoHoursOneLife/OneLifeData7")]
     // one_life_data_directory: String,
-    #[arg(short = 'o', long, default_value = "output.txt")]
+    #[arg(
+        short = 'o',
+        long,
+        default_value = DEFAULT_OUTOUT_FILENAME,
+        help = "Output file to write to. "
+    )]
     output_file: String,
-    #[arg(short = 't', long, default_value = "../../TwoHoursOneLife/twotech")]
-    two_tech_data_directory: String,
+
+    #[arg(short = 't', long, default_value = "../TwoTech-ProcessOutput")]
+    twotech_data_directory: String,
     // Don't worry about this...it's an unfinished option that will possibly be broken out into another program.
     // The idea is to convert an object list into table entries for a wiki page. It's too hard-coded though, and again, should also be broken out.
     #[arg(long, default_value="false")]
@@ -37,13 +48,54 @@ pub struct Args {
     #[arg(long, default_value = "0")]
     min_pickup_age: i32,
     #[arg(long, default_value = "0")]
-    min_slot_size: f32,
+    slot_size: Option<F32Range>,
     #[arg(long, help = "examples: 1, 1000, 0..1, ..2, 4..")]
     num_slots: Option<I32Range>,
 }
 
+fn pause(message: Option<String>) -> bool {
+    let mut stdin = std::io::stdin();
+    let mut stdout = std::io::stdout();
+
+    let default_message = "Type y or yes and ENTER to continue, anything else to exit: ";
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    
+    if let Some(message) = message {
+        println!("{}", message);
+    }
+    // println!("{default_message}");
+    stdout.write(default_message.as_bytes()).unwrap();
+    stdout.flush().unwrap();
+
+    // We want to save the string entered by the user.
+    let mut stdin_data = String::new();
+
+    // Read a single byte and discard
+    let _bytes_read = stdin.read_line(&mut stdin_data).unwrap();
+    let trimmed_stdin_data = stdin_data.trim();
+    if trimmed_stdin_data.len() > 0 {
+        let stdin_data = trimmed_stdin_data.to_lowercase();
+        let stdin_data_str = stdin_data.as_str();
+        if stdin_data_str == "y" || stdin_data_str == "yes" {
+            return true;
+        }
+    }
+    return false;
+}
+
 fn main() -> Result<()> {
     let args = Args::parse();
+    // If the user specified the wiki output option, but didn't specify an output file, the defaul output.json will be misleading.
+    // Warn the user and ask them to say yes to continue.
+    let mut wiki_output_file_check = true;
+    if args.wiki_table_output && args.output_file.as_str() == DEFAULT_OUTOUT_FILENAME {
+        let err_msg = format!("Wiki output selected, but default {DEFAULT_OUTOUT_FILENAME} file still being used. To avoid confusion, perhaps specify a different value with the -o option?");
+        wiki_output_file_check = pause(Some(err_msg));
+    }
+    if !wiki_output_file_check {
+        return Ok(());
+    }
     let mut clothing_to_match = Vec::new();
     if args.clothing.is_some() {
         clothing_to_match = args.clothing.unwrap_or_default().split(",").map(|c| ClothingType::from_str(c).unwrap()).collect::<Vec<_>>();
@@ -52,11 +104,11 @@ fn main() -> Result<()> {
     //     println!("OneLifeData7 directory ({}) could not be opened, please provide different path via the -d option.", args.one_life_data_directory);
     //     return Err(anyhow!(onelife_dir_err));
     // }
-    if let Err(twotech_dir_err) = fs::read_dir(&args.two_tech_data_directory) {
-        println!("TwoTech directory ({}) could not be opened, please provide different path via the -t option.", args.two_tech_data_directory);
+    if let Err(twotech_dir_err) = fs::read_dir(&args.twotech_data_directory) {
+        println!("TwoTech directory ({}) could not be opened, please provide different path via the -t option.", args.twotech_data_directory);
         return Err(anyhow!(twotech_dir_err));
     }
-    let mut two_tech_data_directory = args.two_tech_data_directory;
+    let mut two_tech_data_directory = args.twotech_data_directory;
     if !two_tech_data_directory.ends_with("/") {
         two_tech_data_directory.push('/');
     }
@@ -80,9 +132,16 @@ fn main() -> Result<()> {
         }
     }
 
+    // numSlots filter. Default is all values > 0
     let num_slots_filter = args.num_slots
         .clone()
         .unwrap_or(I32Range(RangeInclusive::new(0, i32::MAX)))
+        .0;
+
+    // slotSize filter. Default is all values
+    let slot_size_filter = args.slot_size
+        .clone()
+        .unwrap_or(F32Range(RangeInclusive::new(f32::MIN, f32::MAX)))
         .0;
 
     let mut objects = objects.iter()
@@ -98,8 +157,8 @@ fn main() -> Result<()> {
             && obj.minPickupAge.unwrap_or(0) >= args.min_pickup_age
             // Number of slots for item falls within specified range (default is all positive values)
             && num_slots_filter.contains(&obj.numSlots.unwrap_or(0))
-            // slotSize is at least as large as the minimum slot size
-            && obj.slotSize.unwrap_or_default() >= args.min_slot_size
+            // slotSize is for item falls within specified range (default is all values allowed)
+            && slot_size_filter.contains(&obj.slotSize.unwrap_or(f32::MIN))
             // object isn't marked as removed
             && !&obj.name.clone().unwrap_or_default().contains("removed")
         })
@@ -148,6 +207,29 @@ impl FromStr for I32Range {
                 let start: i32 = if parts[0].is_empty() { 0 } else { parts[0].parse().map_err(|_| "Invalid number").map_err(|e| anyhow!(e))? };
                 let end: i32 = if parts[1].is_empty() { i32::MAX } else { parts[1].parse().map_err(|_| "Invalid number").map_err(|e| anyhow!(e))? };
                 Ok(I32Range(start..=end))
+            },
+            _ => Err(anyhow!("Invalid range format")),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct F32Range(RangeInclusive<f32>);
+
+impl FromStr for F32Range {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split("..").collect();
+        match parts.len() {
+            1 => {
+                let start: f32 = parts[0].parse().map_err(|_| "Invalid number").map_err(|e| anyhow!(e))?;
+                Ok(F32Range(start..=start))
+            },
+            2 => {
+                let start: f32 = if parts[0].is_empty() { 0.0 } else { parts[0].parse().map_err(|_| "Invalid number").map_err(|e| anyhow!(e))? };
+                let end: f32 = if parts[1].is_empty() { f32::MAX } else { parts[1].parse().map_err(|_| "Invalid number").map_err(|e| anyhow!(e))? };
+                Ok(F32Range(start..=end))
             },
             _ => Err(anyhow!("Invalid range format")),
         }
